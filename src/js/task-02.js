385d1a3d27436d90a0846b597d93dfc242026790
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2023-06-05 21:27:11 +0300

    task 01-06

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsBlock = document.querySelector('#ingredients');
+
+const itemList = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  return item;
+});
+ingredientsBlock.append(...itemList);
